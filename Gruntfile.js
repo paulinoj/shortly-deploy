cb8827347526fa21803cd4cd5e50980f9f2769c2
Hack Reactor Students <accounts@hackreactor.com>
2015-06-10 20:00:55 -0700

    made change to uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
     uglify: {
       my_target: {
         files: {
-          'public/dist/built.min.js' : [ 'public/dist/built.js']
+          'public/dist/built-min.js' : [ 'public/dist/built.js']
         }
       }
     },
